fc311446a01b9a91aa0ddfcd55463e15801b8a4f
Mohammed Atef <mohammed.eldegwy2016@gmail.com>
2022-04-11 19:11:52 +0200

    Lab1 complete

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+   private $posts = [
+       ['id' => 1, 'title' => 'first post', 'posted_by' => 'ahmed', 'created_at' => '2022-04-11'],
+       ['id' => 2, 'title' => 'second post', 'posted_by' => 'mohamed', 'created_at' => '2022-04-11'],
+   ];
+    public function index()
+    {
+
+       return view('posts.index',['allPosts'=>$this->posts]);
+
+
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        $postData=request()->all();
+
+        $post=[
+            "id"=>count($this->posts )+1,
+            "title" => request()["title"],
+            "posted_by" => request()['postedby'],
+            "created_at" => request()['createdat']
+        ];
+
+
+       array_push($this->posts,$post);
+
+
+         return view('posts.index',['allPosts' => $this->posts]);
+
+
+
+    }
+
+    public function show($post)
+    {
+        $post1 = explode(', ', $post);
+
+
+        return view('posts.show',[
+            'post'=>$post1,
+        ]);
+    }
+
+    public function edit($id){
+
+
+        $post=$this->posts[$id];
+
+        return view('posts.edit',['post'=>$post]);
+    }
+
+    public function update($id,Request $request){
+
+        $post=$request->all();
+
+        $this->posts[$id]=$post;
+
+        return view('posts.index',['allPosts' => $this->posts]);
+
+    }
+
+
+    public function destroy ($id){
+
+        unset($this->posts[$id]);
+
+       return view('posts.index',['allPosts' => $this->posts]);
+
+       }
+}
+
