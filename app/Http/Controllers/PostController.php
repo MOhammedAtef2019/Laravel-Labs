faee322079056156b0c747b9a87a900d69238e5b
Mohammed Atef <mohammed.eldegwy2016@gmail.com>
2022-04-14 02:30:55 +0200

    slove math assignment exception by add column to fillable property

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\User;
+use App\Http\Requests\StorePostRequest;
 
 class PostController extends Controller
 {
