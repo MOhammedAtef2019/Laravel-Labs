fef0a08a9234cbd17161e1ff21ac2a946207b5c8
Mohammed Atef <mohammed.eldegwy2016@gmail.com>
2022-04-17 22:58:05 +0200

    add api post controller and make api routes

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\User;
+use App\Http\Resources\PostResource;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+
+        return PostResource::collection($posts);
+        // return Post::all();
+
+    }
+
+    public function show($postId)
+    {
+        $post = Post::find($postId);
+
+        return new PostResource($post);
+    }
+
+    public function store(StorePostRequest $request)
+    {
+        // if(request()->header('Accept') && request()->header('Accept') == 'application/pdf') {
+        //     return ' some pdf';
+        // }
+
+        $data = $request->all();
+
+        //store the request data in the db
+        $post = Post::create([
+            'title' => $data['title'],
+            'description' => $data['description'],
+            'user_id' => $data['post_creator'],
+        ]);
+
+        return new PostResource($post);
+    }
+}
